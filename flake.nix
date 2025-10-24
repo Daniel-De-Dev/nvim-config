2800970ab3dc055899a651be86bd6dc62454d1c0
Daniel-De-Dev <id@daniel-de-dev.com>
2025-10-24 19:01:23 +0200

    refactor: Moved away from home-manager
    Felt weird for the need to have home-manager to have this "auto" config
    setup. This aproach of defining it as an wrapped package feels much
    better and cleaner. I'll keep this aproach for now and see what
    challenges i meet

--- flake.nix
+++ flake.nix
@@ -12,9 +12,10 @@
   };
 
   outputs =
-    inputs@{ ... }:
+    inputs@{ nixpkgs, ... }:
     let
       supportedSystems = [ "x86_64-linux" ];
+      forEachSystem = nixpkgs.lib.genAttrs supportedSystems;
       treefmt = import ./nix/treefmt {
         inherit inputs;
         inherit supportedSystems;
@@ -22,26 +23,34 @@
     in
     treefmt
     // {
-      homeManagerModules.default = { pkgs, ... }:
-      {
-        config = {
+      packages = forEachSystem (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
 
-          home.packages = with pkgs; [
-            ripgrep
-            fd
-            git
-
-            lua-language-server
-            nil
-            nixpkgs-fmt
-            stylua
+          # What programs need to available for nvim
+          nvimDeps = with pkgs; [
           ];
 
-          xdg.configFile."nvim" = {
-            source = ./config;
-            recursive = true;
+          nvimConfigDir = pkgs.stdenv.mkDerivation {
+            name = "nvim-config-dir";
+            src = ./config;
+            dontUnpack = true;
+
+            installPhase = ''
+              mkdir -p $out/nvim
+              cp -r $src/* $out/nvim/
+            '';
           };
-        };
-      };
+
+        in
+        {
+          default = pkgs.writeShellScriptBin "nvim" ''
+            export XDG_CONFIG_HOME=${nvimConfigDir}
+            export PATH=${pkgs.lib.makeBinPath nvimDeps}:$PATH
+            exec ${pkgs.neovim}/bin/nvim "$@"
+          '';
+        }
+      );
     };
 }
