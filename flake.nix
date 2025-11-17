ac7be9b6bd527e8b6f8da46d9c6f33fd481d91f0
Daniel-De-Dev <id@daniel-de-dev.com>
2025-11-17 00:12:27 +0000

    refactor: flake now follows new profile based architecture

--- flake.nix
+++ flake.nix
@@ -1,10 +1,8 @@
 {
-  description = "Neovim Configuration";
+  description = "Neovim Configuration Profiles";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-
-    # Formater
     treefmt-nix = {
       url = "github:numtide/treefmt-nix";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -12,7 +10,7 @@
   };
 
   outputs =
-    inputs@{ nixpkgs, ... }:
+    inputs@{ self, nixpkgs, ... }:
     let
       supportedSystems = [ "x86_64-linux" ];
       forEachSystem = nixpkgs.lib.genAttrs supportedSystems;
@@ -20,36 +18,43 @@
         inherit inputs;
         inherit supportedSystems;
       };
-    in
-    treefmt
-    // {
-      packages = forEachSystem (
-        system:
+
+      mkConfigProfile =
+        {
+          pkgs,
+          profileName,
+          profilePath,
+        }:
         let
-          pkgs = nixpkgs.legacyPackages.${system};
-
-          # What programs need to available for nvim
-          nvimDeps = with pkgs; [
-          ];
-
-          nvimConfigDir = pkgs.stdenv.mkDerivation {
-            name = "nvim-config-dir";
-            src = ./config;
-            dontUnpack = true;
+          profileMeta = import profilePath { inherit pkgs; };
+        in
+        profileMeta
+        // {
+          # Build the config directory from ./profiles/<name>/config
+          dir = pkgs.stdenv.mkDerivation {
+            name = "nvim-config-${profileName}";
+            src = profilePath + "/config";
 
             installPhase = ''
               mkdir -p $out/nvim
               cp -r $src/* $out/nvim/
             '';
           };
-
+        };
+    in
+    treefmt
+    // {
+      configs = forEachSystem (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
         in
         {
-          default = pkgs.writeShellScriptBin "nvim" ''
-            export XDG_CONFIG_HOME=${nvimConfigDir}
-            export PATH=${pkgs.lib.makeBinPath nvimDeps}:$PATH
-            exec ${pkgs.neovim}/bin/nvim "$@"
-          '';
+          personal = mkConfigProfile {
+            inherit pkgs;
+            profileName = "personal";
+            profilePath = ./profiles/personal;
+          };
         }
       );
     };
