8de350fcb39ce91f02e74b99dc683966d299b8d6
Daniel-De-Dev <id@daniel-de-dev.com>
2025-11-21 15:02:52 +0000

    featur(lsp): Expanded lua & lsp config
    Along side configuring lua lsp more, i also restructured the files to
    live in the plugin directory and also utilize lazy nvims opts merging.

---
+++ profiles/personal/config/lua/plugins/lang-lua.lua
@@ -0,0 +1,63 @@
+return {
+	{
+		"neovim/nvim-lspconfig",
+		opts = {
+			lua_ls = {
+				settings = {
+					Lua = {
+						runtime = {
+							version = "LuaJIT",
+						},
+						diagnostics = {
+							disable = { "spell-check" },
+							globals = { "vim" },
+							groupFileStatus = {
+								strong = "Opened",
+								await = "Opened",
+								codestyle = "Opened",
+								luadoc = "Opened",
+							},
+							neededFileStatus = {
+								["global-elements"] = "Opened",
+							},
+							groupSeverity = {
+								["type-check"] = "Error",
+								ambiguity = "Error",
+								global = "Error",
+								unbalanced = "Error",
+								strong = "Error",
+								await = "Error",
+								unused = "Warning",
+								redefined = "Warning",
+								codestyle = "Hint",
+								luadoc = "Information",
+							},
+							severity = {
+								["lowercase-global"] = "Warning",
+								["name-style-check"] = "Information",
+							},
+						},
+						completion = {
+							callSnippet = "Replace",
+						},
+						workspace = {
+							checkThirdParty = false,
+						},
+						telemetry = {
+							enable = false,
+						},
+						type = {
+							weakNilCheck = false,
+						},
+						hint = {
+							enable = true,
+							setType = true,
+							awaitPropagate = true,
+						},
+						format = { enable = false },
+					},
+				},
+			},
+		},
+	},
+}
--- profiles/personal/config/lua/plugins/lazydev.lua
+++ profiles/personal/config/lua/plugins/lazydev.lua
@@ -2,9 +2,6 @@ return {
 	{
 		"folke/lazydev.nvim",
 		ft = "lua",
-		dependencies = {
-			"neovim/nvim-lspconfig", -- relies on it to setup lua_ls
-		},
 		opts = {
 			library = {},
 		},
