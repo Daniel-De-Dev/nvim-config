687ed17071c5ad9287a5355acc2510743607a048
Daniel-De-Dev <id@daniel-de-dev.com>
2025-12-28 01:59:47 +0100

    refactor(rust): Removed "project" spesifc changes
    I moved this "project" spesifc fix over to a .lazy.lua file
    so the global default stay unaffected regarding this issue.

--- profiles/personal/config/lua/plugins/lang-rust.lua
+++ profiles/personal/config/lua/plugins/lang-rust.lua
@@ -3,9 +3,6 @@ return {
     'neovim/nvim-lspconfig',
     opts = {
       rust_analyzer = {
-        -- NOTE: In ESP32 projects it fixed the error where it ends up using
-        -- the rustup versions binary and wont start because of custom toolchain
-        cmd = { "/etc/profiles/per-user/zeus/bin/rust-analyzer" },
         settings = {
           ['rust-analyzer'] = {
             check = {
