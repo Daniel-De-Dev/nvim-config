b2d64a177f8565b2934f56da1433d1e5ccfa8bb6
Daniel-De-Dev <id@daniel-de-dev.com>
2025-12-30 18:00:34 +0100

    feat(rust): removed the forced indent space for rust files

--- profiles/personal/config/lua/plugins/lang-rust.lua
+++ profiles/personal/config/lua/plugins/lang-rust.lua
@@ -1,6 +1,10 @@
 return {
   {
     'neovim/nvim-lspconfig',
+    init = function()
+      -- Disable enforcement of 4-space indent
+      vim.g.rust_recommended_style = 0
+    end,
     opts = {
       rust_analyzer = {
         settings = {
