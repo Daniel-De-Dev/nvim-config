67578e0d6097784fa1b2463230e70c95a17e2503
Daniel-De-Dev <id@daniel-de-dev.com>
2026-01-21 11:33:53 +0100

    refactor: updated python to be stricter

--- profiles/personal/config/lua/plugins/lang-python.lua
+++ profiles/personal/config/lua/plugins/lang-python.lua
@@ -3,6 +3,10 @@ return {
     'neovim/nvim-lspconfig',
     opts = {
       pyright = {
+        analysis = {
+          typeCheckingMode = 'strict',
+          diagnosticMode = 'workspace',
+        },
       },
     },
   },
