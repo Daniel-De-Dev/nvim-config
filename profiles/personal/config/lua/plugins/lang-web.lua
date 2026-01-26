500bb714bf1d04491ed2fa7fc9c298558da0c3ea
Daniel-De-Dev <id@daniel-de-dev.com>
2026-01-26 11:19:26 +0100

    feat: configured css-variables-language-server (needs per project install)

--- profiles/personal/config/lua/plugins/lang-web.lua
+++ profiles/personal/config/lua/plugins/lang-web.lua
@@ -3,9 +3,13 @@ return {
     'neovim/nvim-lspconfig',
     opts = {
       html = {},
-      cssls = {},
       ts_ls = {},
       astro = {},
+      css_variables = {
+        -- NOTE: Has to installed with `npm i -D css-variables-language-server`
+        cmd = { 'node_modules/.bin/css-variables-language-server', '--stdio' },
+        filetypes = { 'css', 'scss', 'less', 'html', 'astro' },
+      },
     },
   },
 }
